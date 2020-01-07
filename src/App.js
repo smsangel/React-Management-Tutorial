2210c849a399fd29bf961f5eb9824edd9210557b
minsu <smsangel77@gmail.com>
2020-01-07 17:50:44 +0900

    Create Custmore.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's debelop management system!!!</h2>
-    </div>
-  );
+const Customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '서민수1',
+  'birthday': '11111',
+  'gender': '여자',
+  'job': '프리랜서'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '서민수2',
+  'birthday': '22222',
+  'gender': '남자',
+  'job': '개인사업'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '서민수3',
+  'birthday': '22222',
+  'gender': '남자',
+  'job': '회사원'
+}]
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        {
+          Customers.map(c => {
+            return (
+              <Customer
+                id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}
+              />
+            )
+          })
+        }
+      </div>      
+    );
+  }
 }
 
 export default App;
