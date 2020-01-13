3360a61a5dbb69039250e15af98c7015e9db2647
minsu <smsangel77@gmail.com>
2020-01-13 15:39:49 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import { withStyles} from '@material-ui/core/styles'; 
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
   root: {
@@ -24,19 +25,26 @@ const styles = theme => ({
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
-    this.callApi()
-    .then(res => this.setState({customers: res}))
-    .catch(err => console.log(err));
+    this.timer = setInterval(this.progress, 20);
+    // this.callApi()
+    // .then(res => this.setState({customers: res}))
+    // .catch(err => console.log(err));
   }
 
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1 });
   }
 
   render() {
@@ -56,7 +64,13 @@ class App extends Component {
           <TableBody>
             {this.state.customers ? this.state.customers.map(c => {
                 return ( <Customer id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
-              }) : ""}
+              }) : 
+               <TableRow>
+                  <TableCell colSpan="6" align="center">
+                      <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+               </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>      
